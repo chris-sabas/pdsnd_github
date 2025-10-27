70d64521bc9965407a07372d25785d45a0f3a22c
chris-sabas <christian.sabas@gmail.com>
2025-10-26 21:33:35 -0600

    Added a comment requesting to update the filenames

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
+## Needs to be updated considering that the data files are in a Data folder
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
